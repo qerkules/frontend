57a42e5bd5ca6dc8153e5f5cd249040f7a647077
syrmon <mmehemmsd@gmail.com>
2024-02-27 15:51:19 +0400

    made little changes

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+import CamryJpg from "../images/camry.jpg";
+import ElantraJpg from "../images/elantra.jpg";
+
+export const cars = [
+  {
+    name: "Toyota Camry",
+    year: "2020",
+    class: "Premium Class",
+    fuel: "Gas",
+    type: "Sedan",
+    price: "70",
+    photo: CamryJpg,
+    link: "cars",
+  },
+  {
+    name: "Hyundai Elantra",
+    class: "Econom Class",
+    year: "2015",
+    fuel: "Gas",
+    type: "Sedan",
+    price: "40",
+    photo: ElantraJpg,
+    link: "cars",
+  },
+  {
+    name: "Hyundai Elantra",
+    class: "Econom Class",
+    year: "2015",
+    fuel: "Gas",
+    type: "Sedan",
+    price: "40",
+    photo: ElantraJpg,
+    link: "cars",
+  },
+  {
+    name: "Hyundai Elantra",
+    class: "Econom Class",
+    year: "2015",
+    fuel: "Gas",
+    type: "Sedan",
+    price: "40",
+    photo: ElantraJpg,
+    link: "cars",
+  },
+];
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,13 +1,13 @@
 import React, { useRef } from "react";
 import styles from "./styles.module.css";
 import Card from "../../components/card/Card";
-import CamryJpg from "../../images/camry.jpg";
-import ElantraJpg from "../../images/elantra.jpg";
+
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import Ourservices from "../../components/ouservices/Ourservices";
 import BrandBanner from "../../components/bradBanner/BrandBanner";
 import Footer from "../../components/footer/Footer";
+import { cars } from "../../data/data";
 
 const Homepage = () => {
   const ref = useRef(null);
@@ -16,64 +16,22 @@ const Homepage = () => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-
-
-  const cars = [
-    {
-      name: "Toyota Camry",
-      year: "2020",
-      class: "Premium Class",
-      fuel: "Gas",
-      type: "Sedan",
-      price: "70",
-      photo: CamryJpg,
-      link: "cars",
-    },
-    {
-      name: "Hyundai Elantra",
-      class: "Econom Class",
-      year: "2015",
-      fuel: "Gas",
-      type: "Sedan",
-      price: "40",
-      photo: ElantraJpg,
-      link: "cars",
-    },
-    {
-      name: "Hyundai Elantra",
-      class: "Econom Class",
-      year: "2015",
-      fuel: "Gas",
-      type: "Sedan",
-      price: "40",
-      photo: ElantraJpg,
-      link: "cars",
-    },
-    {
-      name: "Hyundai Elantra",
-      class: "Econom Class",
-      year: "2015",
-      fuel: "Gas",
-      type: "Sedan",
-      price: "40",
-      photo: ElantraJpg,
-      link: "cars",
-    },
-  ];
-
   return (
     <div className={`${styles.container}`}>
       <Navbar />
       <Header click={handleClick} />
       <Ourservices />
       <BrandBanner />
-      <h4 ref={ref} className={`${styles.cardTitle}`}>
-        Available Cars
-      </h4>
-      <div className={`${styles.cardContainer}`}>
-        {cars.map((car) => {
-          return <Card car={car} />;
-        })}
+      <div className={`${styles.cardSection}`}>
+        <h4 ref={ref} className={`${styles.cardTitle}`}>
+          Available Cars
+        </h4>
+
+        <div className={`${styles.cardContainer}`}>
+          {cars.map((car) => {
+            return <Card car={car} />;
+          })}
+        </div>
       </div>
       <Footer />
     </div>
