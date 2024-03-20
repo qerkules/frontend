89a245839184ca278ea9c06ecd94a75e5c8d390a
syrmon <mmehemmsd@gmail.com>
2024-03-21 00:05:11 +0400

    carpage responsive

--- src/pages/carpage/Carpage.jsx
+++ src/pages/carpage/Carpage.jsx
@@ -36,128 +36,139 @@ const Carpage = () => {
   const bookingComponent = <Booking click={handleClick} />;
   const mainComponent = (
     <div className={`${styles.container}`}>
-      <div className={`${styles.swiperContainer}`}>
-        <Swiper
-          pagination={{
-            type: "fraction",
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation]}
-          className={`${styles.mySwiper}`}
-        >
-          <SwiperSlide className={`${styles.swiperSlide}`}>
-            <img src="https://i.ibb.co/s3mb3w0/porsche.jpg" alt="" className={`${styles.image}`}/>
-          </SwiperSlide>
-          <SwiperSlide className={`${styles.swiperSlide}`}>
-            <img src={ElantraImg} alt="" />
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div className={`${styles.titleSection}`}>
-        <h1 className={`${styles.title}`}>Rent a car in Baku</h1>
-        <h2 className={`${styles.carName}`}>
-          {data && data.brand + " " + data.model + " " + data.year}
-        </h2>
-        <h3 className={`${styles.class}`}>{data && data.class}</h3>
-      </div>
-
-      <div className={`${styles.description}`}>
-        <div className={`${styles.box}`}>
-          <FaCar className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
-          <span>{data && data.mainDetails.type}</span>
-        </div>
-        <div className={`${styles.box}`}>
-          <FaGasPump className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
-          <span>{data && data.mainDetails.fuel}</span>
-        </div>
-        <div className={`${styles.box}`}>
-          <TbManualGearbox className={`${styles.icon}`} />
-          &nbsp;&nbsp;
-          <span>{data && data.mainDetails.transmission}</span>
-        </div>
-        <div className={`${styles.box}`}>
-          <GiCarDoor className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
-          <span>{data && data.mainDetails.doors}</span>
-        </div>
-        <div className={`${styles.box}`}>
-          <PiEngineFill className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
-          <span>{data && data.mainDetails.engine}</span>
-        </div>
-        <div className={`${styles.box}`}>
-          <MdOutlineAirlineSeatReclineExtra className={`${styles.icon}`} />
-          &nbsp;&nbsp;
-          <span>{data && data.mainDetails.seats}</span>
-        </div>
-      </div>
-
-      <div className={`${styles.priceSection}`}>
-        <div>
-          <div className={`${styles.price}`}>
-            &nbsp;$ {data && data.priceUsd}
-            <span className={`${styles.day}`}>/day</span>{" "}
+      <div className={`${styles.desktopVersion}`}>
+        <div className={`${styles.leftSection}`}>
+          <div className={`${styles.swiperContainer}`}>
+            <Swiper
+              pagination={{
+                type: "fraction",
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className={`${styles.mySwiper}`}
+            >
+              <SwiperSlide className={`${styles.swiperSlide}`}>
+                <img
+                  src="https://i.ibb.co/s3mb3w0/porsche.jpg"
+                  alt=""
+                  className={`${styles.image}`}
+                />
+              </SwiperSlide>
+              <SwiperSlide className={`${styles.swiperSlide}`}>
+                <img src={ElantraImg} alt="" />
+              </SwiperSlide>
+            </Swiper>
           </div>
-          <p className={`${styles.tip}`}>
-            *More days will be calculated with discounted price
-          </p>
-        </div>
-        <div
-          className={`${styles.bookNow}`}
-          onClick={() => {
-            handleClick();
-          }}
-        >
-          Book Now
-        </div>
-      </div>
-
-      <div className={`${styles.aboutCar}`}>
-        <h4 className={`${styles.aboutCarTitle}`}>Features</h4>
-
-        <div className={`${styles.carFeature}`}>
-          <Bluetooth className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>Bluetooth</span>
-        </div>
-        <div className={`${styles.carFeature}`}>
-          <UsbInput className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>USB input</span>
-        </div>
-        <div className={`${styles.carFeature}`}>
-          <UsbCharger className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>USB charger</span>
-        </div>
-        <div className={`${styles.carFeature}`}>
-          <HeatedSeat className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>Heated Seats</span>
-        </div>
-        <div className={`${styles.carFeature}`}>
-          <BackCamera className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>Backup camera</span>
-        </div>
-        <div className={`${styles.carFeature}`}>
-          <AllWheel className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>Rear-wheel drive</span>
         </div>
 
-        <div className={`${styles.carFeature}`}>
-          <Insurance className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>Regular Insurance</span>
-        </div>
-        <div className={`${styles.carFeature}`}>
-          <Transmission className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>Automatic transmission</span>
-        </div>
-        <div className={`${styles.carFeature}`}>
-          <Carplay className={`${styles.featureIcon}`} />
-          &nbsp;&nbsp;
-          <span>Apple & Android CarPlay</span>
+        <div className={`${styles.rightSection}`}>
+          <div className={`${styles.titleSection}`}>
+            <h1 className={`${styles.title}`}>Rent a car in Baku</h1>
+            <h2 className={`${styles.carName}`}>
+              {data && data.brand + " " + data.model + " " + data.year}
+            </h2>
+            <h3 className={`${styles.class}`}>{data && data.class}</h3>
+          </div>
+
+          <div className={`${styles.description}`}>
+            <div className={`${styles.box}`}>
+              <FaCar className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
+              <span>{data && data.mainDetails.type}</span>
+            </div>
+            <div className={`${styles.box}`}>
+              <FaGasPump className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
+              <span>{data && data.mainDetails.fuel}</span>
+            </div>
+            <div className={`${styles.box}`}>
+              <TbManualGearbox className={`${styles.icon}`} />
+              &nbsp;&nbsp;
+              <span>{data && data.mainDetails.transmission}</span>
+            </div>
+            <div className={`${styles.box}`}>
+              <GiCarDoor className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
+              <span>{data && data.mainDetails.doors}</span>
+            </div>
+            <div className={`${styles.box}`}>
+              <PiEngineFill className={`${styles.icon}`} /> &nbsp;&nbsp;{" "}
+              <span>{data && data.mainDetails.engine}</span>
+            </div>
+            <div className={`${styles.box}`}>
+              <MdOutlineAirlineSeatReclineExtra className={`${styles.icon}`} />
+              &nbsp;&nbsp;
+              <span>{data && data.mainDetails.seats}</span>
+            </div>
+          </div>
+
+          <div className={`${styles.priceSection}`}>
+            <div>
+              <div className={`${styles.price}`}>
+                &nbsp;$ {data && data.priceUsd}
+                <span className={`${styles.day}`}>/day</span>{" "}
+              </div>
+              <p className={`${styles.tip}`}>
+                *More days will be calculated with discounted price
+              </p>
+            </div>
+            <div
+              className={`${styles.bookNow}`}
+              onClick={() => {
+                handleClick();
+              }}
+            >
+              Book Now
+            </div>
+          </div>
+
+          <div className={`${styles.aboutCar}`}>
+            <h4 className={`${styles.aboutCarTitle}`}>Features</h4>
+
+            <div className={`${styles.carFeature}`}>
+              <Bluetooth className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>Bluetooth</span>
+            </div>
+            <div className={`${styles.carFeature}`}>
+              <UsbInput className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>USB input</span>
+            </div>
+            <div className={`${styles.carFeature}`}>
+              <UsbCharger className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>USB charger</span>
+            </div>
+            <div className={`${styles.carFeature}`}>
+              <HeatedSeat className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>Heated Seats</span>
+            </div>
+            <div className={`${styles.carFeature}`}>
+              <BackCamera className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>Backup camera</span>
+            </div>
+            <div className={`${styles.carFeature}`}>
+              <AllWheel className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>Rear-wheel drive</span>
+            </div>
+
+            <div className={`${styles.carFeature}`}>
+              <Insurance className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>Regular Insurance</span>
+            </div>
+            <div className={`${styles.carFeature}`}>
+              <Transmission className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>Automatic transmission</span>
+            </div>
+            <div className={`${styles.carFeature}`}>
+              <Carplay className={`${styles.featureIcon}`} />
+              &nbsp;&nbsp;
+              <span>Apple & Android CarPlay</span>
+            </div>
+          </div>
         </div>
       </div>
       <div className={`${styles.cardSection}`}>
