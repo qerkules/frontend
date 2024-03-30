e73f31869bd92f0e1a047bec822b9a8dcb991652
syrmon <mmehemmsd@gmail.com>
2024-03-30 12:52:02 +0400

    added map to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Logo from "../../images/logo.png";
+import MapImg from "../../images/map.jpg"
 
 const Footer = () => {
   return (
     <div className={`${styles.container}`}>
       <div className={`${styles.connectionContainer}`}>
-        <div className={`${styles.logoSection}`}>
-          <img src={Logo} alt="" className={`${styles.logo}`} />
-        </div>
-        <div className={`${styles.linksSection}`}>
-          <div className={`${styles.link}`}>About Us</div>
-          <div className={`${styles.link}`}>About Us</div>
-          <div className={`${styles.link}`}>About Us</div>
-        </div>
         <div className={`${styles.contactSection}`}>
+          <img src={Logo} alt="" className={`${styles.logo}`} />
           <div className={`${styles.contactTitle}`}>Contact Options</div>
           <div className={`${styles.contactEmail}`}>mmehemmsd@gmail.com</div>
           <div className={`${styles.contactNumbers}`}>
             <p>+994553151777</p>
           </div>
+        </div>
+        <div className={`${styles.linksSection}`}>
+          <a href="https://maps.app.goo.gl/VNPEHDwREo5ZuVk88">
+            <img src={MapImg} alt=""  className={`${styles.mapImage}`}/>
+          </a>
         </div>
       </div>
       <div className={`${styles.disclaimer}`}>
